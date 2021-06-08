bc32ff08b0b894d120c0b2916d2758e8d14c50db
Adrian <a.marotta97@gmail.com>
2021-06-08 18:34:02 +1000

    Backend functionality updated and almost complete. Outstanding option feature to create QR codes to link business cards.

--- db/dummy_cards.rb
+++ db/dummy_cards.rb
@@ -1,11 +1,11 @@
 require_relative 'helpers.rb'
 
-email = "adrian@gmail.com"
-full_name = "adrian marotta"
+email = "adrian.pro@gmail.com"
+full_name = "Adrian Marotta"
 logo = "http://pngimg.com/uploads/google/google_PNG19635.png"
 mobile = '+61418249781'
-website = "https://www.google.com/"
-business_address = "48 Pirrama Rd, Pyrmont NSW 2009, Australia"
+website = "https://www.adrian.dev.com/"
+business_address = "VIC"
 
 sql = "INSERT INTO card_info (users_id, email, full_name, logo, mobile, website, business_address) VALUES ('1', '#{email}', '#{full_name}', '#{logo}', #{mobile}, '#{website}', '#{business_address}')"
 
--- main.rb
+++ main.rb
@@ -19,10 +19,19 @@ def logged_in?
   !!session[:user_id]
 end
 
+def saved_cards?
+  if session[:user_id] == nil 
+    return {}
+  end
+
+  sql = ("SELECT saved_cards FROM users WHERE id = $1")
+  run_sql(sql, [session[:user_id]])[0] 
+end
+
 get '/' do
+  cards = run_sql("SELECT * FROM card_info;")
 
-
-  erb :index
+  erb :index, locals: { cards:cards }
 end
 
 get '/login' do
@@ -50,7 +59,7 @@ end
 
 get '/cards' do
   cards = run_sql("SELECT * FROM card_info WHERE users_id = #{session[:user_id].to_i}")
-  pp session[:user_id].to_i
+
   erb :cards, locals: { cards:cards }
 end
 
@@ -73,6 +82,8 @@ post '/cards' do
     params["card_website"],
     params["card_address"]
   ])
+
+  
 
   redirect '/'
 end
@@ -115,6 +126,24 @@ put '/cards/:id' do
     params['id']
   ])
 
-  # redirect
   redirect "/cards/#{params["id"]}"
+end
+
+get '/saved_cards' do
+
+  saved_cards = run_sql("SELECT saved_cards FROM users WHERE id = #{current_user()['id']}")
+  saved_cards = saved_cards.to_a
+  saved_cards = saved_cards[0]["saved_cards"]
+
+  results = run_sql("SELECT * FROM card_info WHERE id = ANY('#{saved_cards}'::int[]);")
+  results = results.to_a
+  erb :saved_cards, locals: { results:results }
+end
+
+put '/cards/:id/save' do
+  sql = ("UPDATE users SET saved_cards = array_append(saved_cards, $1) WHERE id = #{current_user()['id']};")
+
+  run_sql(sql, [params['id']])
+
+  redirect "/saved_cards"
 end
